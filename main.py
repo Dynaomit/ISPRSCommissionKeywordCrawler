efdaeebbc42f90cad0a818d9c143037b6f30c4cc
dynaomit <smiky2247@googlemail.com>
2024-02-12 15:51:10 +0100

    added force directed graph, refactoring

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import pickle
 import re
-from copy import copy
+from itertools import combinations
+
+from tqdm import tqdm
+from fuzzywuzzy import fuzz
 
 import matplotlib.style
 import pandas as pd
 from bs4 import BeautifulSoup
 import httplib2
-import matplotlib.pyplot as plt
 from collections import Counter
-from Levenshtein import ratio as levenshtein_distance
-import plotly.express as px
+import plotly.graph_objects as go
 
 
 def request_website(link_string):
@@ -24,12 +26,13 @@ def get_commission_links():
     """
     links = []
     response = request_website("https://www.isprs.org/publications/annals.aspx")
-    for link in BeautifulSoup(response, features="html.parser").find_all('a'):
+    for link in tqdm(BeautifulSoup(response, features="html.parser").find_all('a'),
+                     desc="Parsing Website for Commissions"):
         link_ref = link.get('href')
         link_name = link.text
         if link_ref is not None \
                 and link_name is not None \
-                and "www.isprs-ann-photogramm-remote-sens-spatial-inf-sci.net" in link_ref \
+                and "isprs-annals.copernicus.org" in link_ref \
                 and "Commission" in link_name \
                 and "Technical" not in link_name:
             links.append(link_ref)
@@ -43,7 +46,7 @@ def get_index_links_from_commission_links(commission_links):
     :return: list of links to keyword indexes
     """
     index_links = []
-    for commission_link in commission_links:
+    for commission_link in tqdm(commission_links, desc="Processing Commissions"):
         response = request_website(commission_link)
         for sub_link in BeautifulSoup(response, features="html.parser").find_all('a'):
             link_name = sub_link.text
@@ -67,6 +70,27 @@ def find_year_in_string(text):
         return None
 
 
+keyword_mapping = {}
+
+
+def get_umbrella_term(keyword):
+    global keyword_mapping
+
+    # Check if the keyword is already in the mapping
+    if keyword in keyword_mapping:
+        return keyword_mapping[keyword]
+
+    # Find the most similar existing keyword
+    for existing_keyword in keyword_mapping:
+        if fuzz.ratio(keyword, existing_keyword) >= 80:
+            return keyword_mapping[existing_keyword]
+
+    # If no similar keyword is found, set the keyword as its own umbrella term
+    keyword_mapping[keyword] = keyword
+
+    return keyword
+
+
 def get_keywords_and_counts_basic(index_links):
     """
     from a given list of links referring to keyword indexes,
@@ -76,8 +100,9 @@ def get_keywords_and_counts_basic(index_links):
     :return: dictionary with keywords as keys and amounts as values, amounts are seperated into each commission's year
     """
     keywords = dict()
+    papers = dict()
     current_keyword = ""
-    for index_link in index_links:
+    for index_link in tqdm(index_links, desc="Processing Keyword Indices"):
         response = request_website(index_link)
         soup = BeautifulSoup(response, features="html.parser")
         current_year = find_year_in_string(soup.text)
@@ -87,68 +112,78 @@ def get_keywords_and_counts_basic(index_links):
         for div in mydivs:
             for content in div.contents:
                 if content.name == "span":
-                    current_keyword = re.sub('\s+', ' ', content.text).strip().lstrip().lower()
+                    current_keyword = content.get_text(strip=True)
                 if content.name == "ul":
-                    if current_keyword not in keywords:
-                        keywords[current_keyword] = {current_year: 1}
-                    else:
-                        if current_year not in keywords[current_keyword]:
-                            keywords[current_keyword][current_year] = 1
+                    paper_split = content.get_text(strip=False).split('\n')
+                    paper_split = [paper for paper in paper_split if paper.strip() != ""]
+                    for paper in paper_split:
+                        keyword_to_add = current_keyword
+                        if paper not in papers:
+                            papers[paper] = [keyword_to_add]
+                        if keyword_to_add not in papers[paper]:
+                            papers[paper].append(keyword_to_add)
+                        if current_keyword not in keywords:
+                            keywords[current_keyword] = {current_year: 1}
                         else:
-                            keywords[current_keyword][current_year] += 1
-    return keywords
+                            if current_year not in keywords[current_keyword]:
+                                keywords[current_keyword][current_year] = 1
+                            else:
+                                keywords[current_keyword][current_year] += 1
+    # Flatten the list of keywords from all papers
+    all_keywords = list(set(k for ks in papers.values() for k in ks))
+
+    # Determine the threshold for co-occurrence
+    threshold = 3  # Adjust this value as per your requirement
+
+    # Find pairs of keywords that co-occur more than the threshold
+    cooccurrences = Counter()
+    for ks in papers.values():
+        keyword_combinations = combinations(ks, 2)
+        cooccurrences.update(keyword_combinations)
+
+    frequent_pairs = {pair: count for pair, count in cooccurrences.items() if count >= threshold}
+
+    # Sort the frequent pairs by frequency in descending order
+    sorted_pairs = sorted(frequent_pairs.items(), key=lambda x: x[1], reverse=True)
+
+    return keywords, sorted_pairs
 
 
 def plot_keywords(keyword_dict, top_n):
     pd.options.plotting.backend = "plotly"
     matplotlib.style.use('tableau-colorblind10')
     top = dict(sorted(keyword_dict.items(), key=lambda x: sum(x[1].values()), reverse=True)[:top_n])
+    #top = dict(sorted(keyword_dict.items(), key=lambda x: x[1].get('2022', 0), reverse=True)[:top_n])
     with open(r'./keywords.txt', 'w') as fp:
         for key in top.keys():
-            fp.write("%s\n" % key)
+            fp.write("%s\n" % (key[0].upper() + key[1:]))
     for key in list(top.keys()):
         new_key = key.strip('][').split(', ')[0].strip('\'')
+        new_key = new_key[0].upper() + new_key[1:]
         top[new_key] = top[key]
         del top[key]
     df = pd.DataFrame(top).transpose()
     df = df.iloc[:, ::-1]
     df.sort_index(axis=0, ascending=False)
-    fig = df.plot(kind='bar')#, stacked=True)
+    fig = df.plot(kind='bar')
     fig.update_layout(
         title="Top {} keywords".format(top_n),
         xaxis_title="Keyword",
         yaxis_title="Amount mentioned",
-        legend_title="Commission Years"
+        legend_title="Congress Years",
+        title_font=dict(size=40),
+        xaxis=dict(
+            tickfont=dict(size=20),
+            title_font=dict(size=20)
+        ),
+        yaxis=dict(
+            tickfont=dict(size=20),
+            title_font=dict(size=20)
+        ),
+        legend=dict(font=dict(size=20))
     )
     fig.show()
     fig.write_html("top{}keywords_plotly.html".format(top_n))
-    #plt.xticks(rotation=45, ha='right', rotation_mode='anchor', fontsize=4)
-    #plt.yticks(fontsize=7)
-    # plt.yticks(new_list)
-    #plt.tight_layout()
-    #plt.savefig('Top{} Keywords'.format(top_n), dpi=500)
-    #plt.show()
-
-
-def sync_counts(k1, k2):
-    for key in k2.keys():
-        if key not in k1.keys():
-            k1[key] = k2[key]
-        else:
-            k1[key] += k2[key]
-    return k1
-
-
-def get_overlap(s1, s2):
-    """
-    calculates the overlap of two strings on a character level, ignores dashes and spaces
-    :param s1: first string
-    :param s2: second string
-    :return: the overlapping
-    """
-    string_list_1 = s1.replace('-', ' ').split(' ')
-    string_list_2 = s2.replace('-', ' ').split(' ')
-    return list(set(string_list_1).intersection(string_list_2))
 
 
 def group_keys(keyword_dict):
@@ -162,36 +197,15 @@ def group_keys(keyword_dict):
     groups = list()
     for first_key in keyword_dict.keys():
         for group in groups:
-            if all(levenshtein_distance(first_key, w) > 0.8 for w in group):
+            #if all(fuzz.ratio(first_key, w) >= 80 for w in group):
+            #    group.append(first_key)
+            #    break
+            if fuzz.ratio(first_key, group[0]) >= 80:
                 group.append(first_key)
                 break
         else:
             groups.append([first_key, ])
     return groups
-
-
-def process_duplicates(keyword_dict):
-    keys_to_delete = []
-    for i, key in enumerate(keyword_dict.keys()):
-        for j, other_key in enumerate(keyword_dict.keys()):
-            dist = 0  # levenshtein_ratio_and_distance(key, other_key, ratio_calc=True)
-            if i != j and dist > 0.8:
-                overlap = get_overlap(key, other_key).strip()
-                if overlap != key and overlap != other_key and overlap in keyword_dict.keys():
-                    new_values = dict(
-                        Counter(keyword_dict[key]) + Counter(keyword_dict[other_key]) + Counter(keyword_dict[overlap]))
-                    keyword_dict[overlap] = new_values
-                    keys_to_delete.append(key)
-                    keys_to_delete.append(other_key)
-                else:
-                    keyword_dict[overlap] = dict(Counter(key) + Counter(other_key))
-                    if overlap == key:
-                        keys_to_delete.append(other_key)
-                    else:
-                        keys_to_delete.append(key)
-    for key_to_delete in list(dict.fromkeys(keys_to_delete)):
-        keyword_dict.pop(key_to_delete)
-    return keyword_dict
 
 
 def calculate_grouped_amounts(keyword_dict, grouped_keywords):
@@ -210,14 +224,161 @@ def calculate_grouped_amounts(keyword_dict, grouped_keywords):
     return group_dict
 
 
+def process_pair_keywords(keywords, pair_frequencies):
+    test = pair_frequencies.copy()
+    sum1 = 0
+    sum2 = 0
+    for i, (pair, frequency) in enumerate(pair_frequencies):
+        keyword1, keyword2 = pair
+        for key in keywords.keys():
+            keyword_group = eval(key)
+            if keyword1 in keyword_group:
+                keyword1 = keyword_group[0]
+                sum1 = sum(keywords[key].values())
+            if keyword2 in keyword_group:
+                keyword2 = keyword_group[0]
+                sum2 = sum(keywords[key].values())
+        pair_frequencies[i] = ((keyword1, keyword2), frequency, (sum1, sum2))
+
+    duplicate_free_pairs = {}
+
+    # Iterate over the data list
+    for pair, frequency, sums in pair_frequencies:
+        # Check if the pair exists in the combined_data dictionary
+        if pair in duplicate_free_pairs:
+            # If the pair exists, add the frequency to the existing value
+            duplicate_free_pairs[pair][0] += frequency
+            # Update the sums
+            assert(duplicate_free_pairs[pair][1] == sums)
+        else:
+            # If the pair doesn't exist, create a new key-value pair
+            duplicate_free_pairs[pair] = [frequency, sums]
+
+    # Convert the dictionary back to a list of pairs
+    duplicate_free_pairs = [(pair, frequency, sums) for pair, (frequency, sums) in duplicate_free_pairs.items()]
+    duplicate_free_pairs.sort(key=lambda x: x[1], reverse=True)
+    return duplicate_free_pairs
+
+
+def plot_map(pair_frequencies):
+    with open("pair_frequencies.pkl", "wb") as f:
+        pickle.dump(pair_frequencies, f)
+    import networkx as nx
+
+    # Create an empty graph
+    graph = nx.Graph()
+    # Add edges with pair frequencies as an attribute
+    for (keyword1, keyword2), frequency, (sum1, sum2) in pair_frequencies:
+        k1 = keyword1[0].upper() + keyword1[1:]
+        k2 = keyword2[0].upper() + keyword2[1:]
+        graph.add_node(k1, weight=sum1*0.5)
+        graph.add_node(k2, weight=sum2*0.5)
+        graph.add_edge(k1, k2, frequency=frequency)
+
+    # Define node positions for the force-directed graph
+    layout = nx.spring_layout(graph)
+
+    # Extract node positions
+    node_positions = {node: (pos[0], pos[1]) for node, pos in layout.items()}
+
+    # Define edge traces
+    edge_traces = []
+    for edge in graph.edges():
+        x0, y0 = node_positions[edge[0]]
+        x1, y1 = node_positions[edge[1]]
+        frequency = graph.edges[edge]["frequency"]
+        edge_trace = go.Scatter(
+            x=[x0, x1, None],
+            y=[y0, y1, None],
+            mode="lines",
+            line=dict(width=frequency, color="gray"),
+            hoverinfo="none",
+        )
+        edge_traces.append(edge_trace)
+
+    # Define node traces
+    node_traces = []
+    for node in graph.nodes():
+        x, y = node_positions[node]
+        frequency = graph.nodes[node]["weight"]*2
+        node_trace = go.Scatter(
+            x=[x],
+            y=[y],
+            mode="markers",
+            marker=dict(size=frequency, color="skyblue", line=dict(color='black', width=1)),
+            text=f"{node} ({frequency})",
+            hoverinfo="text",
+        )
+        node_traces.append(node_trace)
+
+        label_trace = go.Scatter(
+            x=[x],
+            y=[y],
+            mode="text",
+            text=[node],
+            textposition="middle center",
+            hoverinfo="none",
+            showlegend=False,
+            textfont=dict(
+                color="black",
+                size=12
+            )
+        )
+        node_traces.append(label_trace)
+
+    # Create figure
+    figure = go.Figure(data=edge_traces + node_traces)
+
+    # Update layout
+    figure.update_layout(
+        title="Force-Directed Graph For Keyword Relation",
+        showlegend=False,
+        hovermode="closest",
+        xaxis=dict(visible=False),
+        yaxis=dict(visible=False),
+        title_font=dict(size=40),
+    )
+
+    # Show the graph
+    figure.show()
+    figure.write_html("keyword_frequency_and_relation_map.html")
+    return pair_frequencies
+
+
 def main():
     commission_links = get_commission_links()
     index_links = get_index_links_from_commission_links(commission_links)
-    keyword_dict = get_keywords_and_counts_basic(index_links)
+    keyword_dict, sorted_frequency_pairs = get_keywords_and_counts_basic(index_links)
+    filtered_keys = [key for key in keyword_dict.keys() if
+                     '/' not in key and '\\' not in key and '<' not in key and '>' not in key]
+    filtered_empty_dict = {key: {} for key in filtered_keys}
+    with open('filtered_keys.pkl', 'wb') as f:
+        pickle.dump(list(filtered_empty_dict), f)
     grouped_keywords = group_keys(keyword_dict)
     grouped_keyword_dict = calculate_grouped_amounts(keyword_dict, grouped_keywords)
+    print("Performed grouping optimization")
     print("Keys saved by grouping: ", len(keyword_dict.keys()) - len(grouped_keyword_dict.keys()))
-    plot_keywords(grouped_keyword_dict, 30)
+    plot_keywords(grouped_keyword_dict, 100)
+    grouped_pair_frequencies = process_pair_keywords(grouped_keyword_dict, sorted_frequency_pairs)
+    #for i in range(10):
+    plot_map(grouped_pair_frequencies)
+    plot_frequency_pairs(grouped_pair_frequencies)
+
+
+def plot_frequency_pairs(sorted_frequency_pairs):
+    keywords = [f"{pair[0][0]}, {pair[0][1]}" for pair in sorted_frequency_pairs]
+    frequencies = [pair[1] for pair in sorted_frequency_pairs]
+    with open("keywords_for_graph.pkl", 'wb') as f:
+        pickle.dump(keywords, f)
+    with open("frequencies_for_graph.pkl", 'wb') as f:
+        pickle.dump(frequencies, f)
+    fig = go.Figure(data=go.Scatter(x=keywords, y=frequencies, mode='markers'))
+    fig.update_layout(title='Keyword Pair Frequencies',
+                      xaxis_title='Keyword Pair',
+                      yaxis_title='Frequency',
+                      title_font=dict(size=40))
+    fig.show()
+    fig.write_html("frequency_pairs.html")
 
 
 if __name__ == '__main__':
